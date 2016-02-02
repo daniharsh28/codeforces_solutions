1d40357f82ad35a655df2236a18c8cd03ddbd300
Harsh Dani <daniharsh28@gmail.com>
2016-02-01 18:01:31 -0700

    Adding random teams problem

---
+++ codeforces_random_teams.py
@@ -0,0 +1,34 @@
+import os,sys
+
+inp = raw_input().split()
+inp = [int(x) for x in inp]
+
+n = inp[0]
+m = inp[1]
+
+if m == 1:
+	maxiInOneCluster = minInOneCluster = n * (n-1) / 2
+	print minInOneCluster, maxiInOneCluster
+elif m == n:
+	print 0, (m -n) * (m - n - 1)/2
+else:
+	# For min try to divide as evenly as possible
+	minInCluster = n / m 
+	entriesInCluster = [n/m for _ in xrange(m)]
+
+	for i in xrange(n % m):
+		entriesInCluster[i] += 1
+
+	totalMin = 0
+	for entry in entriesInCluster:
+		if entry >= 2:
+			totalMin += entry * (entry - 1) / 2
+
+	# For maximum try to assign 1 to each cluster
+	maxiInOneCluster = n - (m-1)
+
+	print totalMin , maxiInOneCluster * (maxiInOneCluster - 1)/2
+
+
+
+
