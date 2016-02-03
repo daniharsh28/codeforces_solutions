f41ef6ba9207121057cf506352dca42f0c7e42f1
Harsh Dani <daniharsh28@gmail.com>
2016-02-03 13:26:48 -0700

    Adding digits sum problem

---
+++ codeforces_digit_sum.py
@@ -0,0 +1,11 @@
+import os,sys
+
+inp = raw_input().split()
+inp = [int(x) for x in inp]
+
+n = inp[0]
+m = inp[1]
+
+#Based on the numbers we should decide start and end
+start = 1 * (10 * (n-1))
+end = 9 * (11 * )
