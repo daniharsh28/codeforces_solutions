75cdef09db2e163809b1067acc9db9fe396173e5
Harsh Dani <daniharsh28@gmail.com>
2016-02-04 22:14:04 -0700

    Adding icpc problem

---
+++ codeforces_icpc.py
@@ -0,0 +1,16 @@
+import os,sys
+
+inp = raw_input().split()
+inp = [int(x) for x in inp]
+
+n = inp[0]
+k = inp[1]
+
+arr = raw_input().split()
+arr = [int(x) for x in inp]
+
+arr.sort()
+for i in range(0,len(arr),3):
+	x = arr[i+2]
+	if (x + k < 5):
+		
