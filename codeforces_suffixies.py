2396fcd44cec8e3eac0fdc11598491fe8ce9e201
Harsh Dani <daniharsh28@gmail.com>
2016-02-08 10:05:41 -0700

    Accepted solution of suffixies problem

--- codeforces_suffixies.py
+++ codeforces_suffixies.py
@@ -9,23 +9,17 @@ m = inp[1]
 arr = raw_input().split()
 arr = [int(x) for x in arr]
 
-database = dict()
-for i in xrange(len(arr)):
-	if arr[i] in database.keys():
-		database[arr[i]] += 1
-	else:
-		database[arr[i]] = 1
+database = set()
+count = []
+for i in xrange(len(arr)-1,-1,-1):
+	database.add(arr[i])
+	count.append(len(database))
 
+print count
 #print database
 
 for _ in range(m):
 	query_index = int(raw_input())
-	
-	temp_database = database.copy()
-	
-	for i in xrange(query_index - 1):
-		if temp_database[arr[i]] != 0:
-			temp_database[arr[i]] -= 1
-
+		
 	#print database
-	print sum([1 for k,v in temp_database.items() if (v > 0)])
+	print count[len(arr) - query_index]
