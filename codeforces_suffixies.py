3e6517a93b800adba8df6624ff232ad4ddb23fff
Harsh Dani <daniharsh28@gmail.com>
2016-02-08 00:40:46 -0700

    Changing suffixies to dictionary instead of set

--- codeforces_suffixies.py
+++ codeforces_suffixies.py
@@ -9,10 +9,23 @@ m = inp[1]
 arr = raw_input().split()
 arr = [int(x) for x in arr]
 
+database = dict()
+for i in xrange(len(arr)):
+	if arr[i] in database.keys():
+		database[arr[i]] += 1
+	else:
+		database[arr[i]] = 1
+
+#print database
+
 for _ in range(m):
 	query_index = int(raw_input())
-	counter = 0
-	database = set()
-	for i in range(query_index-1,len(arr)):
-		database.add(arr[i])
-	print len(database)
+	
+	temp_database = database.copy()
+	
+	for i in xrange(query_index - 1):
+		if temp_database[arr[i]] != 0:
+			temp_database[arr[i]] -= 1
+
+	#print database
+	print sum([1 for k,v in temp_database.items() if (v > 0)])
